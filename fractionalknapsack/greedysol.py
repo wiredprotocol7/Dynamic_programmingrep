ce142052ac8064412f3117e366bc32a033553678
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2020-11-16 17:36:09 +0530

    Create greedysol.py

---
+++ fractionalknapsack/greedysol.py
@@ -0,0 +1,3 @@
+#Fraction Knapsack is a greedy problem. It is not a DP problem but a greedy problem. 
+#Combinational Optimization
+#Decision makers have to choose from a set of non-divisible projects or tasks under a fixed budget or time constraint, respectively. 
