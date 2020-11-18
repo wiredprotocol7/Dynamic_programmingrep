07c6bad4111fbcc750248567cdda0ec232248105
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2020-11-18 14:09:35 +0530

    Update recursion.cpp
    Solution Link: https://stackoverflow.com/a/37850446/9380247

--- unbounded_knapsack/combinational_sum/recursion.cpp
+++ unbounded_knapsack/combinational_sum/recursion.cpp
@@ -1,1 +1,30 @@
 
+long long int countrecur(long long int n, long long int min)
+{   
+    if (n==0)
+    {
+        return 1;     
+    }
+    else if(n<0)
+    {
+        return 0;
+    }
+    else 
+    {
+        long long int ans=0;
+        if(min<=3)
+        {ans+=countrecur(n-3,3);}
+        //cout<<"fp"<<endl;
+        if(min<=5)
+        {ans+=countrecur(n-5,5);}
+        if(min<=10)
+        {ans+=countrecur(n-10,10);}
+        return ans;
+    }
+
+}
+long long int count(long long int n)
+{  
+    
+   return countrecur(n,0);
+}
